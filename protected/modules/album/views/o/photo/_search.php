d2bbbc98e9a005268ef87119020075a6b32d3a13
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-09-14 01:13:23 +0700

    albums: commit

--- protected/modules/album/views/o/photo/_search.php
+++ protected/modules/album/views/o/photo/_search.php
@@ -4,6 +4,7 @@
  * @var $this PhotoController
  * @var $model AlbumPhoto
  * @var $form CActiveForm
+ * version: 0.1.4
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
@@ -49,8 +50,13 @@
 		</li>
 
 		<li>
-			<?php echo $model->getAttributeLabel('desc'); ?><br/>
-			<?php echo $form->textArea($model,'desc',array('rows'=>6, 'cols'=>50)); ?>
+			<?php echo $model->getAttributeLabel('title'); ?><br/>
+			<?php echo $form->textArea($model,'title',array('rows'=>6, 'cols'=>50)); ?>
+		</li>
+
+		<li>
+			<?php echo $model->getAttributeLabel('description'); ?><br/>
+			<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
 		</li>
 
 		<li>
